ef128c4b71486965f2acd08e8f868ac1a354932b
hoijui <hoijui.quaero@gmail.com>
2016-10-18 11:03:52 +0200

    allow to easily investigate changes in one input variable with a slider
    Only works for integer values as of now!

--- src/main/java/com/siemens/industrialbenchmark/TrialGuiMain.java
+++ src/main/java/com/siemens/industrialbenchmark/TrialGuiMain.java
@@ -24,23 +24,40 @@ import info.monitorenter.gui.chart.Chart2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.beans.PropertyChangeEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BoundedRangeModel;
 import javax.swing.DefaultBoundedRangeModel;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
 
 /**
  * A comfortable GUI to play around with a random simulation.
  */
 public class TrialGuiMain extends javax.swing.JFrame {
+
+	private static final String PRIMARY_INPUT_VAR = "STATIONARY_SETPOINT";
+	private static final String PRIMARY_OUTPUT_VAR = "RewardTotal";
 
 	private Map<String, List<Double>> states;
 	private final PropertiesTable simPropsTable;
@@ -53,18 +70,102 @@ public class TrialGuiMain extends javax.swing.JFrame {
 
 		this.setSize(1024, 600);
 
-		this.simPropsTable = new PropertiesTable(loadDefaultSimulationProperties());
+		final Properties simulationProps = loadDefaultSimulationProperties();
+		this.simPropsTable = new PropertiesTable(simulationProps);
 		this.simPropsSP.getViewport().add(simPropsTable);
 
-		this.runB.setAction(new SimulationRunAction());
-
-		final DefaultComboBoxModel<String> stateKeysModel = new DefaultComboBoxModel<>();
-		stateKeysModel.removeAllElements();
-		for (final String key : MarkovianStateDescription.getNonConvolutedInternalVariables()) {
-			stateKeysModel.addElement(key);
+		final DefaultComboBoxModel<String> inputVarsKeysModel = new DefaultComboBoxModel<>();
+		inputVarsKeysModel.removeAllElements();
+		final Enumeration<Object> simulationPropKeys = simulationProps.keys();
+		while (simulationPropKeys.hasMoreElements()) {
+			inputVarsKeysModel.addElement((String) simulationPropKeys.nextElement());
 		}
-		this.shownStateKeyCB.setModel(stateKeysModel);
-		this.shownStateKeyCB.addItemListener(new KeyChangeListener());
+		this.propNumberSliderCB.setModel(inputVarsKeysModel);
+		final InputVarKeyChangeListener inputVarKeyChangeListener = new InputVarKeyChangeListener();
+		inputVarKeyChangeListener.setFromTo(PRIMARY_INPUT_VAR, 0, 100); // HACK
+		this.propNumberSliderCB.addItemListener(inputVarKeyChangeListener);
+		this.propNumberSliderCB.setSelectedItem(PRIMARY_INPUT_VAR);
+
+		final SimulationRunAction simulationRunAction = new SimulationRunAction();
+
+		this.propNumberSliderS.addChangeListener(new InputVarSliderChangeListener(simulationRunAction));
+		this.propNumberSliderS.setMajorTickSpacing(10);
+		this.propNumberSliderS.setMinorTickSpacing(5);
+
+		final FromToChangeListener fromToChangeListener = new FromToChangeListener();
+		addChangeListener(this.propNumberSliderFrom, fromToChangeListener);
+		addChangeListener(this.propNumberSliderTo, fromToChangeListener);
+
+		this.runB.setAction(simulationRunAction);
+
+		final DefaultComboBoxModel<String> outputStateKeysModel = new DefaultComboBoxModel<>();
+		outputStateKeysModel.removeAllElements();
+		for (final String key : MarkovianStateDescription.getNonConvolutedInternalVariables()) {
+			outputStateKeysModel.addElement(key);
+		}
+		this.shownStateKeyCB.setModel(outputStateKeysModel);
+		this.shownStateKeyCB.setSelectedItem(PRIMARY_OUTPUT_VAR);
+		this.shownStateKeyCB.addItemListener(new OutputVarKeyChangeListener());
+
+		simulationRunAction.actionPerformed(null);
+	}
+
+	/**
+	 * Installs a listener to receive notification when the text of any
+	 * {@code JTextComponent} is changed. Internally, it installs a
+	 * {@link DocumentListener} on the text component's {@link Document}, and a
+	 * {@link PropertyChangeListener} on the text component to detect if the
+	 * {@code Document} itself is replaced.
+	 *
+	 * @param text any text component, such as a {@link JTextField} or
+	 * {@link JTextArea}
+	 * @param changeListener a listener to receive {@link ChangeEvent}s when
+	 * the text is changed; the source object for the events will be the text
+	 * component
+	 * @throws NullPointerException if either parameter is null
+	 */
+	public static void addChangeListener(final JTextComponent text, final ChangeListener changeListener) {
+		Objects.requireNonNull(text);
+		Objects.requireNonNull(changeListener);
+		DocumentListener docLst = new DocumentListener() {
+			private int lastChange = 0, lastNotifiedChange = 0;
+
+			@Override
+			public void insertUpdate(final DocumentEvent evt) {
+				changedUpdate(evt);
+			}
+
+			@Override
+			public void removeUpdate(final DocumentEvent evt) {
+				changedUpdate(evt);
+			}
+
+			@Override
+			public void changedUpdate(final DocumentEvent evt) {
+				lastChange++;
+				SwingUtilities.invokeLater(() -> {
+					if (lastNotifiedChange != lastChange) {
+						lastNotifiedChange = lastChange;
+						changeListener.stateChanged(new ChangeEvent(text));
+					}
+				});
+			}
+		};
+		text.addPropertyChangeListener("document", (PropertyChangeEvent evt) -> {
+			Document doc1 = (Document) evt.getOldValue();
+			Document doc2 = (Document) evt.getNewValue();
+			if (doc1 != null) {
+				doc1.removeDocumentListener(docLst);
+			}
+			if (doc2 != null) {
+				doc2.addDocumentListener(docLst);
+			}
+			docLst.changedUpdate(null);
+		});
+		Document doc = text.getDocument();
+		if (doc != null) {
+			doc.addDocumentListener(docLst);
+		}
 	}
 
 	private static Properties loadDefaultSimulationProperties() {
@@ -92,10 +193,101 @@ public class TrialGuiMain extends javax.swing.JFrame {
 		return (Integer) nSimStepsS.getValue();
 	}
 
-	private class KeyChangeListener implements ItemListener {
+	private class InputVarKeyChangeListener implements ItemListener {
+
+		private final Map<String, Map<Object, Object>> propKeyFromTo;
+
+		public InputVarKeyChangeListener() {
+
+			this.propKeyFromTo = new HashMap<>();
+		}
+
+		public void setFromTo(final String propName, final Object from, final Object to) {
+
+			propKeyFromTo.put(propName, Collections.singletonMap(from, to));
+		}
 
 		@Override
-		public void itemStateChanged(ItemEvent event) {
+		public void itemStateChanged(final ItemEvent event) {
+
+			if (event.getStateChange() != ItemEvent.SELECTED) {
+				return;
+			}
+
+			final String previouslySelectedKey = (String) propNumberSliderCB.getSelectedItem();
+			setFromTo(previouslySelectedKey, propNumberSliderFrom.getText(), propNumberSliderTo.getText());
+
+			final String newlySelectedKey = (String) event.getItem();
+			final Map<Object, Object> fromTo
+					= propKeyFromTo.containsKey(newlySelectedKey)
+					? propKeyFromTo.get(newlySelectedKey)
+					: Collections.singletonMap(0, 100);
+			final Map.Entry<Object, Object> fromToEntry = fromTo.entrySet().iterator().next();
+			propNumberSliderFrom.setText(fromToEntry.getKey().toString());
+			propNumberSliderTo.setText(fromToEntry.getValue().toString());
+//			propNumberSliderS.getModel().setRangeProperties(vale, 1, from, to, false);
+			final String valueRaw = simPropsTable.getProperties().getProperty(newlySelectedKey);
+			boolean sliderSuport = false;
+			try {
+				propNumberSliderS.setValue(Double.valueOf(valueRaw).intValue());
+				sliderSuport = true;
+			} catch (final NumberFormatException ex) {
+				ex.printStackTrace();
+				sliderSuport = false;
+			}
+			propNumberSliderFrom.setEnabled(sliderSuport);
+			propNumberSliderS.setEnabled(sliderSuport);
+			propNumberSliderTo.setEnabled(sliderSuport);
+		}
+	}
+
+	private class InputVarSliderChangeListener implements ChangeListener {
+
+		private final Action action;
+
+		public InputVarSliderChangeListener(final Action action) {
+
+			this.action = action;
+		}
+
+		@Override
+		public void stateChanged(final ChangeEvent event) {
+
+			if (propNumberSliderS.getValueIsAdjusting()) {
+				return;
+			}
+
+			final int newValue = propNumberSliderS.getValue();
+			final String selectedProp = (String) propNumberSliderCB.getSelectedItem();
+			simPropsTable.setProperty(selectedProp, String.valueOf(newValue));
+
+			action.actionPerformed(null);
+		}
+	}
+
+	private class FromToChangeListener implements ChangeListener {
+
+		@Override
+		public void stateChanged(final ChangeEvent event) {
+
+			try {
+				final Double from = Double.valueOf(propNumberSliderFrom.getText());
+				final Double to = Double.valueOf(propNumberSliderTo.getText());
+				propNumberSliderS.setMinimum((int) Math.ceil(from));
+				propNumberSliderS.setMaximum((int) Math.floor(to));
+
+				propNumberSliderS.setMajorTickSpacing((int) ((to - from) / 10));
+				propNumberSliderS.setMinorTickSpacing((int) ((to - from) / 20));
+			} catch (final NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(), "Failed to set from & to", JOptionPane.WARNING_MESSAGE);
+			}
+		}
+	}
+
+	private class OutputVarKeyChangeListener implements ItemListener {
+
+		@Override
+		public void itemStateChanged(final ItemEvent event) {
 
 			if (states == null) {
 				return;
@@ -172,6 +364,11 @@ public class TrialGuiMain extends javax.swing.JFrame {
         westP = new javax.swing.JPanel();
         simPropsP = new javax.swing.JPanel();
         simPropsSP = new javax.swing.JScrollPane();
+        propNumberSliderP = new javax.swing.JPanel();
+        propNumberSliderCB = new javax.swing.JComboBox<>();
+        propNumberSliderFrom = new javax.swing.JTextField();
+        propNumberSliderS = new javax.swing.JSlider();
+        propNumberSliderTo = new javax.swing.JTextField();
         chartSP = new javax.swing.JScrollPane();
         bottomP = new javax.swing.JPanel();
         simulationProgressPB = new javax.swing.JProgressBar();
@@ -188,6 +385,25 @@ public class TrialGuiMain extends javax.swing.JFrame {
         simPropsP.setBorder(javax.swing.BorderFactory.createTitledBorder("Simulation Properties"));
         simPropsP.setLayout(new java.awt.BorderLayout());
         simPropsP.add(simPropsSP, java.awt.BorderLayout.CENTER);
+
+        propNumberSliderP.setLayout(new java.awt.BorderLayout());
+
+        propNumberSliderCB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        propNumberSliderP.add(propNumberSliderCB, java.awt.BorderLayout.NORTH);
+
+        propNumberSliderFrom.setText("0");
+        propNumberSliderFrom.setPreferredSize(new java.awt.Dimension(60, 23));
+        propNumberSliderP.add(propNumberSliderFrom, java.awt.BorderLayout.WEST);
+
+        propNumberSliderS.setPaintLabels(true);
+        propNumberSliderS.setPaintTicks(true);
+        propNumberSliderP.add(propNumberSliderS, java.awt.BorderLayout.CENTER);
+
+        propNumberSliderTo.setText("100");
+        propNumberSliderTo.setPreferredSize(new java.awt.Dimension(60, 23));
+        propNumberSliderP.add(propNumberSliderTo, java.awt.BorderLayout.EAST);
+
+        simPropsP.add(propNumberSliderP, java.awt.BorderLayout.SOUTH);
 
         westP.add(simPropsP, java.awt.BorderLayout.CENTER);
 
@@ -262,6 +478,11 @@ public class TrialGuiMain extends javax.swing.JFrame {
     private javax.swing.JPanel controllsP;
     private javax.swing.JSplitPane mainSP;
     private javax.swing.JSpinner nSimStepsS;
+    private javax.swing.JComboBox<String> propNumberSliderCB;
+    private javax.swing.JTextField propNumberSliderFrom;
+    private javax.swing.JPanel propNumberSliderP;
+    private javax.swing.JSlider propNumberSliderS;
+    private javax.swing.JTextField propNumberSliderTo;
     private javax.swing.JButton runB;
     private javax.swing.JComboBox<String> shownStateKeyCB;
     private javax.swing.JPanel simPropsP;
