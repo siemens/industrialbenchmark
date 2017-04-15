32856f64a23e0a06df5d3a98c2f38f71c6a3ec38
hoijui <hoijui.quaero@gmail.com>
2017-04-15 09:51:47 +0200

    DataVectorImpl: cleanup
    Conflicts:
    	src/main/java/com/siemens/industrialbenchmark/datavector/DataVectorImpl.java

--- src/main/java/com/siemens/industrialbenchmark/datavector/DataVectorImpl.java
+++ src/main/java/com/siemens/industrialbenchmark/datavector/DataVectorImpl.java
@@ -27,7 +27,6 @@ import com.siemens.rl.interfaces.DataVector;
  * This class holds a HashMap of state-dimension/action-dimension -value pairs.
  *
  * @author Michel Tokic
- *
  */
 public class DataVectorImpl implements Cloneable, DataVector {
 
@@ -36,39 +35,41 @@ public class DataVectorImpl implements Cloneable, DataVector {
 	 */
 	private static final long serialVersionUID = 4956886314253943518L;
 
-	private HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
-	private double values[];
+	private final HashMap<String, Integer> indexMap;
+	private double[] values;
 
-	DataVectorDescription description = null;
-	ImmutableList<String> keys;
+	private final DataVectorDescription description;
+	private final ImmutableList<String> keys;
 
-	/**
-	 * Initializes the state with a given StateVectorDescription. All associated values are set to NaN initially.
-	 * @param desc The StateVectorDescription
-	 */
-	public DataVectorImpl(DataVectorDescription desc) {
-		Preconditions.checkNotNull(desc, "Description must not be null.");
-		this.description = desc;
-		this.keys = ImmutableList.copyOf(desc.getVarNames());
-		values = new double[this.keys.size()];
-		for (int i=0; i<this.keys.size(); i++) {
-			indexMap.put(this.keys.get(i), i);
-			values[i] = Double.NaN;
+	private DataVectorImpl(final DataVectorDescription description, final List<String> keys) {
+		this.description = description;
+		this.keys = ImmutableList.copyOf(keys);
+		this.indexMap = new HashMap<String, Integer>();
+		this.values = new double[this.keys.size()];
+		for (int i=0; i < this.keys.size(); i++) {
+			this.indexMap.put(this.keys.get(i), i);
+			this.values[i] = Double.NaN;
 		}
 	}
 
 	/**
-	 * Initializes the state with a given list of state dimension names. All associated values are set to NaN initially.
+	 * Initializes the state with a given StateVectorDescription.
+	 * All associated values are set to <code>NaN</code> initially.
+	 * @param description The StateVectorDescription
+	 */
+	public DataVectorImpl(final DataVectorDescription description) {
+		this(description, description.getVarNames());
+		Preconditions.checkNotNull(description, "Description must not be null.");
+	}
+
+	/**
+	 * Initializes the state with a given list of state dimension names.
+	 * All associated values are set to <code>NaN</code> initially.
 	 * @param keys A list of keys.
 	 */
-	public DataVectorImpl(List<String> keys) {
-		Preconditions.checkNotNull(keys, "Description must not be null.");
-		this.keys = ImmutableList.copyOf(keys);
-		values = new double[this.keys.size()];
-		for (int i=0; i<this.keys.size(); i++) {
-			indexMap.put(this.keys.get(i), i);
-			values[i] = Double.NaN;
-		}
+	public DataVectorImpl(final List<String> keys) {
+		this(null, keys);
+		Preconditions.checkNotNull(keys, "Keys must not be null.");
 	}
 
 	/**
@@ -76,6 +77,7 @@ public class DataVectorImpl implements Cloneable, DataVector {
 	 * @param key The state dimension
 	 * @return The value
 	 */
+	@Override
 	public Double getValue(String key) {
 		Preconditions.checkArgument(this.getKeys().contains(key), "%s is not a valid variable", key);
 
@@ -91,6 +93,7 @@ public class DataVectorImpl implements Cloneable, DataVector {
 	 * @param key The state/action dimension
 	 * @param value The value
 	 */
+	@Override
 	public void setValue(String key, double value) {
 		Preconditions.checkNotNull(this.getKeys(), "keySet is null!!");
 		Preconditions.checkArgument(this.getKeys().contains(key), "%s is not a valid variable. Available names are: %s", key, this.getKeys());
@@ -101,6 +104,7 @@ public class DataVectorImpl implements Cloneable, DataVector {
 	 * returns a list containing the state/action dimension names
 	 * @return a list containing the state/action dimension names
 	 */
+	@Override
 	public List<String> getKeys() {
 		return this.keys;
 	}
@@ -121,12 +125,13 @@ public class DataVectorImpl implements Cloneable, DataVector {
 	 * returns a double[] array containing the values
 	 * @return a double[] array containing the values
 	 */
+	@Override
 	public double[] getValuesArray() {
-		double values[] = new double[keys.size()];
+		double[] valuesCopy = new double[keys.size()];
 		for (int i=0; i<this.keys.size(); i++) {
-			values[i] = this.values[indexMap.get(keys.get(i))];
+			valuesCopy[i] = this.values[indexMap.get(keys.get(i))];
 		}
-		return values;
+		return valuesCopy;
 	}
 
     @Override
@@ -145,6 +150,7 @@ public class DataVectorImpl implements Cloneable, DataVector {
     	return output;
     }
 
+	@Override
     public DataVector clone() {
 		DataVector s = new DataVectorImpl(this.getKeys());
     	for (String key : this.getKeys()) {
